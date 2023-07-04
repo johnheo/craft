8a6d94c0b63f4bcf4306f16ac396e8f9b0a580bd
SEYEDARMIN AZIZI <armin.az77@yahoo.com>
2023-07-03 22:40:24 -0700

    Update __init__.py

--- finetune/models/__init__.py
+++ finetune/models/__init__.py
@@ -1,1 +1,11 @@
+import copy
+import torch
+import torchvision.models as torch_models
+from . import cifar10 as cifar10_models
+from . import cifar100 as cifar100_models
+from . import flowers102 as flowers102_models
+from . import stanfordcars as stanfordcars_models
+from . import oxford-iiit as oxfordpets_models
+from . import fgvc-aircraft as aircraft_models
+from . import food101 as food101_models
 
