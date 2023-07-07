2b8ad869d499c9ac2b3c32e8cc08caab4a261d4e
SEYEDARMIN AZIZI <armin.az77@yahoo.com>
2023-07-06 23:22:49 -0700

    Update ViT.py

--- finetune/models/cifar10/ViT.py
+++ finetune/models/cifar10/ViT.py
@@ -3,10 +3,10 @@ import torch.nn as nn
 import timm
 
 
-__all__ = ['vit']
+__all__ = ['vit_cifar10']
 
 
-def vit():
+def vit_cifar10():
   
    model =  timm.create_model('vit_base_patch16_224_in21k', pretrained = True)
    model.head = nn.Linear(768, 10, bias = True)
