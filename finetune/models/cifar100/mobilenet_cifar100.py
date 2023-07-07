d97a91c3e34dc3df5dffb9ed67870bc18916afd1
SEYEDARMIN AZIZI <armin.az77@yahoo.com>
2023-07-06 23:36:12 -0700

    Create mobilenet_cifar100.py

---
+++ finetune/models/cifar100/mobilenet_cifar100.py
@@ -0,0 +1,15 @@
+import timm
+import torch
+import torch.nn as nn
+
+
+
+__all__ = ["mobilenetv2_cifar100"]
+
+
+
+def mobilenetv2_cifar100()
+    model = timm.create_model('mobilenetv2_100', pretrained = True)
+    model.classifier = nn.Linear(1280, 100, True)
+    return model
+
