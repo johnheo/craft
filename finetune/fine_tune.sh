96f267eb3c0bbbd285f462096e514b584b1180c2
SEYEDARMIN AZIZI <armin.az77@yahoo.com>
2023-08-11 00:28:12 -0700

    Update fine_tune.sh

--- finetune/fine_tune.sh
+++ finetune/fine_tune.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python compress_classifier.py -a vit_oxfordpets -j 4 --epochs 10 -b 8 -p 15 --wd 0.01 --gpus 0 -o logs --deterministic --vs 0.0 --confusion --lr 1e-4 --compress sched.yaml /data/datasets/oxfordpets
+python compress_classifier.py -a vit_oxfordpets -j 4 --epochs 10 -b 8 -p 15 --wd 0.01 --gpus 0 -o logs --deterministic --vs 0.0 --confusion --lr 1e-4 --compress sched.yaml --dataset oxfordpets /data/datasets/oxfordpets
