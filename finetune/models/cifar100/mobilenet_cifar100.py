19eb31511605287521c2e5b3121acbd5e94c435c
SEYEDARMIN AZIZI <armin.az77@yahoo.com>
2023-08-10 17:38:07 -0700

    Update mobilenet_cifar100.py

--- finetune/models/cifar100/mobilenet_cifar100.py
+++ finetune/models/cifar100/mobilenet_cifar100.py
@@ -8,7 +8,7 @@ __all__ = ["mobilenetv2_cifar100"]
 
 
 
-def mobilenetv2_cifar100()
+def mobilenetv2_cifar100():
     model = timm.create_model('mobilenetv2_100', pretrained = True)
     model.classifier = nn.Linear(1280, 100, True)
     return model
