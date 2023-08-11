dd33577406239ab0651acd814f60a96fdbd7b027
SEYEDARMIN AZIZI <armin.az77@yahoo.com>
2023-08-11 00:28:44 -0700

    Update __init__.py

--- finetune/apputils/__init__.py
+++ finetune/apputils/__init__.py
@@ -1,7 +1,9 @@
 from .data_loaders import *
 from .checkpoint import *
 from .execution_env import *
+from .performance_tracker import *
 
 del data_loaders
 del checkpoint
 del execution_env
+del performance_tracker
