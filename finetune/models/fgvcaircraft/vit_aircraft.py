7b4053018b8941b89374d165452aa725167b5364
SEYEDARMIN AZIZI <armin.az77@yahoo.com>
2023-08-18 20:49:08 -0700

    Update vit_aircraft.py

--- finetune/models/fgvcaircraft/vit_aircraft.py
+++ finetune/models/fgvcaircraft/vit_aircraft.py
@@ -1,1 +1,13 @@
+import timm
+import torch
+import torch.nn as nn
 
+__all__ = ['vit_aircraft']
+
+
+def vit_aircraft():
+
+   model =  timm.create_model('vit_base_patch16_224_in21k', pretrained = True)
+   model.head = nn.Linear(768, 100, bias = True)
+
+   return model
